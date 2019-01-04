b0c2613c35f564cfe8e0eddb1298a89dd2e454e9
dean <deanpm1994@gmail.com>
2019-01-04 00:18:57 -0500

    Adding sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './test.png';
 import './App.scss';
+
+import About from './components/about/';
 
 class App extends Component {
   render() {
@@ -8,17 +10,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Header.scss';
+// import './Header.scss';
 class Header extends Component {
     state = {  }
     render() { 

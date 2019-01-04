49e6fdfa59b9310b354f0b2fd1664c678282637b
dean <deanpm1994@gmail.com>
2019-01-04 10:25:59 -0500

    Including JQuery and make parallax effect in logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
-import logo from './test.png';
-import './App.scss';
+import React, { Component } from "react";
+import logo from "./logotest.svg";
+import "./App.scss";
+import $ from "jquery";
 
-import About from './components/about/About';
-import Services from './components/services/Services';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 class App extends Component {
+  componentDidMount() {
+    $(window).scroll(function() {
+      var yPos = $(window).scrollTop() / 10;
+      var newPos = 35 + yPos;
+      $(".App-logo").css({
+        top: newPos + "%"
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        </header>{" "}
         <About />
         <Services />
         <Contact />

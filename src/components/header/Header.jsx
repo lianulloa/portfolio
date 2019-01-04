92feeaf4c44a4e8b092d77f7a0603907438e31e3
dean <deanpm1994@gmail.com>
2019-01-03 23:24:32 -0500

    initial changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-
+import './Header.scss';
 class Header extends Component {
     state = {  }
     render() { 
-        return (  );
+        return (
+            <header className="Header">
+                
+            </header>
+        );
     }
 }
  

580494d186017a3cc020975074d9d6af6126c6ec
dean <deanpm1994@gmail.com>
2019-01-04 01:51:21 -0500

    Introducing other changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Description from '../description/Description';
 
 class About extends Component {
     state = {  }
     render() { 
         return ( 
             <div className="App-section">
-
+                <Description />
             </div>
         );
     }
---
+++ src/components/description/Description.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Description extends Component {
+    render() { 
+        return (  
+            <div>
+                <h1>How we are?</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta blanditiis fugiat est neque a qui ex facere possimus voluptatem quos unde magnam, deserunt itaque tempora, explicabo sed officiis, veniam animi!</p>
+            </div>
+        );
+    }
+}
+ 
+export default Description;

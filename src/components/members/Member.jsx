f2e1cf8c1c5fbe23f04d376705b4abbcaabd50fe
dean <deanpm1994@gmail.com>
2019-01-06 22:17:12 -0500

    Tomorrow is another day

--- src/components/members/Member.jsx
+++ src/components/members/Member.jsx
@@ -1,29 +1,28 @@
 import React, { Component } from "react";
-import { Card, CardMedia } from "@material-ui/core";
 import "./Member.scss";
 
 class Member extends Component {
   state = {};
   render() {
-    let { thumb, name, resume, social } = this.props.member;
+    const { thumb, name, resume, social } = this.props.member;
     let styles = {
-      width: "100%",
-      height: "200px",
-      display: "block",
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
-      backgroundImage: 'url("'.concat(thumb, '")')
+      backgroundImage: 'url("'.concat(thumb, '")'),
+      height: 0,
+      paddingTop: "56.25%"
     };
 
     return (
-      <div>
-        <div className="member-card">
-          <div className="card-image" style={styles} />
+      <div className="member-card">
+        <div className="card-image" style={styles} />
+        <div className="card-content">
+          <h4 className="card-name">{name}</h4>
+          <p className="card-resume">{resume}</p>
         </div>
-        <Card>
-          <CardMedia image={thumb} />
-        </Card>
+        <div className="card-social">
+          {social[0]}
+          {social[1]}
+          {social[2]}
+        </div>
       </div>
     );
   }

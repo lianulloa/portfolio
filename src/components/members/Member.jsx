38a42e95c11a9d9c46d8825328da81f3c155ad8a
dean <deanpm1994@gmail.com>
2019-01-06 21:56:54 -0500

    Styling profile photo

--- src/components/members/Member.jsx
+++ src/components/members/Member.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
+import { Card, CardMedia } from "@material-ui/core";
 import "./Member.scss";
 
 class Member extends Component {
   state = {};
   render() {
     let { thumb, name, resume, social } = this.props.member;
+    let styles = {
+      width: "100%",
+      height: "200px",
+      display: "block",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundImage: 'url("'.concat(thumb, '")')
+    };
 
     return (
-      <div className="member-card">
-        <div className="card-image">
-          <img src={thumb} alt={name + "photo"} />
+      <div>
+        <div className="member-card">
+          <div className="card-image" style={styles} />
         </div>
+        <Card>
+          <CardMedia image={thumb} />
+        </Card>
       </div>
     );
   }

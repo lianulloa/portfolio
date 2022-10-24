675042b02dc635dc354b12a36fa4fbb787d17f7d
Lian Ulloa <29025113+lianulloa@users.noreply.github.com>
2022-10-24 23:07:53 +0200

    wip: Do not use three.js on mobile yet

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import Footer from "./components/footer/Footer"
 import NavBar from "./components/navBar/NavBar"
 import RouterView from "./components/routerView/RouterView"
 import { renderBackground, cleanUp } from "./utils/threejs/pageBackground"
+import device from "./utils/device"
 import "./App.scss"
 function App() {
   const canvas = useRef(null)
   useEffect(() => {
-    renderBackground(canvas.current)
+    !device.is("phone") && renderBackground(canvas.current)
 
     return cleanUp
   }, [])
 
   return (
     <div className="App">
-      <canvas ref={canvas} width={innerWidth} height={innerHeight} className="page-background kk" />
+      <canvas ref={canvas} width={innerWidth} height={innerHeight} className="page-background" />
       {true &&
         <React.Fragment>
           <NavBar />

e1684bf1ba86a14fe00ee99b18077c77f54adb05
Lian Ulloa <29025113+lianulloa@users.noreply.github.com>
2022-10-24 18:26:53 +0200

    feat: Implement hover effect

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React, {useEffect, useRef} from "react"
 import Footer from "./components/footer/Footer"
 import NavBar from "./components/navBar/NavBar"
 import RouterView from "./components/routerView/RouterView"
-import { renderBackground } from "./utils/threejs/pageBackground"
+import { renderBackground, cleanUp } from "./utils/threejs/pageBackground"
 import "./App.scss"
 function App() {
   const canvas = useRef(null)
   useEffect(() => {
     renderBackground(canvas.current)
+
+    return cleanUp
   }, [])
 
   return (
     <div className="App">
-      <canvas ref={canvas} width={innerWidth} height={innerHeight} className="page-background" />
+      <canvas ref={canvas} width={innerWidth} height={innerHeight} className="page-background kk" />
       {true &&
         <React.Fragment>
           <NavBar />
--- src/utils/threejs/pageBackground.js
+++ src/utils/threejs/pageBackground.js
@@ -1,5 +1,20 @@
 import * as THREE from "three"
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import gsap from "gsap"
+
+// const planeColor = 0x3668a5
+const planeColorArray = [54/255, 104/255, 165/255]
+const planeAccentColorArray = [138 / 255, 234 / 255, 146 / 255]
+
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
+const captureNormalizedMousePosition = e => {
+  mouse.x = (e.clientX / innerWidth) * 2 - 1
+  mouse.y = -((e.clientY / innerHeight) * 2 - 1)
+}
 
 let width, height
 export const renderBackground = (canvas) => {
@@ -9,32 +24,36 @@ export const renderBackground = (canvas) => {
   renderer.setSize(width, height)
   renderer.setClearColor(0xffffff, 0)
 
+  const raycaster = new THREE.Raycaster()
+
   const scene = new THREE.Scene()
 
   const camera = new THREE.PerspectiveCamera(75, canvas.width/canvas.height, 0.1, 1000)
   camera.position.z = 35
   scene.add(camera)
 
-  // const cubeGeometry = new THREE.BoxGeometry(10, 10, 10)
-  // const cubeMaterial = new THREE.MeshBasicMaterial({ color: "white"})
   const planeGeometry = new THREE.PlaneGeometry(60, 60, 20, 20)
   const planeMaterial = new THREE.MeshPhongMaterial({
-    color: 0x3668a5,
+    // color: planeColor,
     flatShading: true,
-    // vertexColors: true
+    vertexColors: true
   })
   const plane = new THREE.Mesh(planeGeometry, planeMaterial)
   scene.add(plane)
+  //TODO: Enable after implementing raycasting
   plane.rotation.set(-1.0, 0.8, 0.0)
   plane.position.x = 5
+  const colors = Array(plane.geometry.attributes.position.count).fill(planeColorArray)
+  plane.geometry.setAttribute("color", new THREE.BufferAttribute(
+    new Float32Array(
+      [].concat(...colors)
+    ),
+    3
+  ))
 
-  // console.log(plane)
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableZoom = false
 
-  // const controls = new OrbitControls(camera, renderer.domElement)
-  // controls.enableZoom = false
-
-  // const light = new THREE.PointLight(0xffffff)
-  // light.position.set(-10, 15, 50)
   const light = new THREE.DirectionalLight(0xffffff, 1)
   light.position.set(0, 0, 10)
   scene.add(light)
@@ -46,15 +65,56 @@ export const renderBackground = (canvas) => {
       array[i] += Math.random() * 2
     }
   }
-
   bringPlaneToLife()
 
   const render = () => {
-    // requestAnimationFrame(render)
+    requestAnimationFrame(render)
     renderer.render(scene, camera)
-    // controls.update()
-  }
+    controls.update()
 
+    raycaster.setFromCamera(mouse, camera)
+    const intersects = raycaster.intersectObject(plane)
+    if (intersects.length > 0) {
+      lightIntersection(intersects[0])
+    }
+    // console.log(intersects)
+  }
+  const lightIntersection = (intersect) => {
+    const {face} = intersect
+    // for (const key of ["a", "b", "c"]) {
+    //   intersect.object.geometry.attributes.color.setX(face[key], planeAccentColorArray[0])
+    //   intersect.object.geometry.attributes.color.setY(face[key], planeAccentColorArray[1])
+    //   intersect.object.geometry.attributes.color.setZ(face[key], planeAccentColorArray[2])
+    // }
+    // intersect.object.geometry.attributes.color.needsUpdate = true
+
+    const hoverColor = {
+      r: planeAccentColorArray[0],
+      g: planeAccentColorArray[1],
+      b: planeAccentColorArray[2]
+    }
+
+    gsap.to(hoverColor, {
+      r: planeColorArray[0],
+      g: planeColorArray[1],
+      b: planeColorArray[2],
+      duration: 1.5,
+      onUpdate: () => {
+        for (const key of ["a", "b", "c"]) {
+          intersect.object.geometry.attributes.color.setX(face[key], hoverColor.r)
+          intersect.object.geometry.attributes.color.setY(face[key], hoverColor.g)
+          intersect.object.geometry.attributes.color.setZ(face[key], hoverColor.b)
+        }
+        intersect.object.geometry.attributes.color.needsUpdate = true
+      }
+    })
+  }
+  
   render()
+  addEventListener("mousemove", captureNormalizedMousePosition)
+}
+
+export const cleanUp = () => {
+  removeEventListener("mousemove", captureNormalizedMousePosition)
 }
 

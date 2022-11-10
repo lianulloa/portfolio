41e0690fca022767b5cd032a81c6b12c7e6b6aa9
Lian Ulloa <29025113+lianulloa@users.noreply.github.com>
2022-11-10 01:55:31 +0100

    fix: Catch frontend routes

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import svgrPluging from "vite-plugin-svgr"
@@ -6,7 +7,13 @@ export default defineConfig({
   plugins: [react(), svgrPluging()],
   base: "/portfolio/",
   build: {
-    outDir: "build"
+    outDir: "build",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "404": resolve(__dirname, "404.html")
+      }
+    }
   },
   server: {
     open: true

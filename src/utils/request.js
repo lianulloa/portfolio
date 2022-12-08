47c0fcb0695a64ceb4b82bce47d7d0bd270a8a75
Lian Ulloa <29025113+lianulloa@users.noreply.github.com>
2022-12-08 22:57:09 +0100

    Clean up

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { getToken } from "./session"
 
 const service = axios.create({
-  baseURL: process.env.NODE_ENV === "pdevelopment"
+  baseURL: process.env.NODE_ENV === "development"
     ? "http://localhost:8080/api/"
     : "https://portfolio-backend-production-9167.up.railway.app/api/",
   timeout: 30000,
